ae44919a8a37562e74b1d2bc0ae552d30698b9b0
VAGRAMCHIC <vagramchic@gmail.com>
2022-06-25 12:40:07 +0300

    add telegram nitifications

--- init_env.py
+++ init_env.py
@@ -16,13 +16,18 @@ def get_env_variable(env_var_name: str):
         return os.getenv(env_var_name)
 
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 CURRCONV_API = 'https://free.currconv.com/api/v7/convert?q=USD_RUB&compact=ultra&apiKey='
 CURRCONV_API_KEY = get_env_variable('CURRCONV_API_KEY')
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = get_env_variable('SPREADSHEET_ID')
 RANGE_NAME = get_env_variable('RANGE_NAME')
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+
+DB_BOT_TABLE=get_env_variable('DB_BOT_TABLE')
+TG_API_TOKEN=get_env_variable('TG_API_TOKEN')
+TG_BROADCAST_DELAY=int(get_env_variable('TG_BROADCAST_DELAY'))
 
 TIMESTAMP=int(get_env_variable('TIMESTAMP'))
 
